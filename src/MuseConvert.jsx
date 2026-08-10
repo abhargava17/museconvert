d914aa1d1c101c80fc045e0d140d6c3c10ea8da3
abhargava17 <archit8bhargava@gmail.com>
2026-08-10 15:16:33 -0500

    Refactor import statements in MuseConvert.jsx

--- src/MuseConvert.jsx
+++ src/MuseConvert.jsx
@@ -1,8 +1,7 @@
 "use client";
 
 
-import { useState, useRef, useCallback } from "react";
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
